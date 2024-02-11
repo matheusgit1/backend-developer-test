a708930ad0b57c1abae9bf5db8379fa3e3c82550
Mathues Alves <mapereira@nuageit.com.br>
2024-02-11 15:44:31 -0300

    feat: add jobs controlers

---
+++ api-jobs/src/controllers/jobs.controller.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import { PgCliente } from "../infrastructure/database/client/database.repository";
+import { JobsUseCase } from "../usecases/jobs";
+import { HandlerEvents } from "../infrastructure/services/handle-eventos.service";
+
+const routes = Router();
+
+const usecase = new JobsUseCase(new PgCliente(), new HandlerEvents());
+
+routes.post(
+  "/",
+  async (_req, _res, _next) => await usecase.createJob(_req, _res, _next)
+);
+
+routes.put(
+  "/:job_id/publish",
+  async (_req, _res, _next) => await usecase.publishJob(_req, _res, _next)
+);
+
+routes.put(
+  "/:job_id",
+  async (_req, _res, _next) => await usecase.editJob(_req, _res, _next)
+);
+
+routes.delete(
+  "/:job_id",
+  async (_req, _res, _next) => await usecase.deleteJob(_req, _res, _next)
+);
+
+routes.put(
+  "/:job_id/archive",
+  async (_req, _res, _next) => await usecase.archiveJob(_req, _res, _next)
+);
+
+export { routes };
