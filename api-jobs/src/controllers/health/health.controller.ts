1292ec4b7e9b1800b8979598930d908d47f3406c
Mathues Alves <mapereira@nuageit.com.br>
2024-02-12 16:12:14 -0300

    reafactor: refactoring controller

---
+++ api-jobs/src/controllers/health/health.controller.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { GetHealthUseCase } from "../../usecases/health/gethealth.usecase";
+import { adaptRoute } from "../adapters";
+import { Usecases } from "../shareds";
+
+export class HealthRoutesAdapted {
+  public routes: Router = Router();
+  constructor(private readonly usecases: Usecases) {
+    for (const { method, path, usecase } of this.usecases) {
+      this.routes[method](
+        path,
+        async (req: Request, res: Response, _next: NextFunction) =>
+          await adaptRoute(() => usecase.handler({ req }), res)
+      );
+    }
+  }
+}
+
+const routesAdapteds = new HealthRoutesAdapted([
+  {
+    path: "/",
+    method: "get",
+    usecase: new GetHealthUseCase(),
+  },
+]);
+
+const routes = routesAdapteds.routes;
+
+export { routes };
