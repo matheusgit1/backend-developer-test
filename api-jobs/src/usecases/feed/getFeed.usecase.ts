6f7230b8ad317b9032bf013ad1af780df5c46522
Mathues Alves <mapereira@nuageit.com.br>
2024-02-20 10:37:15 -0300

    feat: add docstring

--- api-jobs/src/usecases/feed/getFeed.usecase.ts
+++ api-jobs/src/usecases/feed/getFeed.usecase.ts
@@ -15,6 +15,41 @@ export class GetFeedUseCase implements BaseUseCase {
   ) {}
   public async handler({ req: _req }: { req: Request }): Promise<HttpResponse> {
     try {
+      /**
+       * @description NOTA. Apesar de parecer mais interesseante num primeito momento o uso direto do json, a medida
+       * que o volume de dados cresce, é importante o uso de processamento de dados sob demando.
+       * qunatos menos pesado for o json no s3, mas rapido o endpoint retornara para  o cliente a lista que ele quer, mas...
+       * em um cenário de muitas requisições com um json muito pesado, fica inviavel essa implementação.
+       * Aqui em baixo tem um exemplo de implementação de entrega de conteudo do json sob demanda, simulando web socket
+       * use as nodejs stream para tratar e processar e entregar sob demanda os dados.
+       */
+      // str
+      //   .pipeThrough(
+      //     new TransformStream({
+      //       transform(chunk, controller) {
+      //         const d = JSON.parse(
+      //           JSON.stringify(
+      //             String.fromCharCode.apply(
+      //               null,
+      //               new Uint8Array(Buffer.from(chunk).toJSON().data)
+      //             )
+      //           )
+      //         );
+      //         console.log(d);
+      //         controller.enqueue(d);
+      //       },
+      //     })
+      //   )
+      //   .pipeTo(
+      //     new WritableStream({
+      //       async write(chunk) {
+      //         res.write(chunk);
+      //       },
+      //       close() {
+      //         res.end();
+      //       },
+      //     })
+      //   );
       const feedInCache = this.cache.get<string>(cacheKey);
 
       if (feedInCache) {
